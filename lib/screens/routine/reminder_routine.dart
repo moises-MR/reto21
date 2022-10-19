657c0711024c4d000fcd04b9431f1456ecd7bbc8
Moises Ramirez <moises@wbox.mx>
2022-10-19 07:33:14 -0500

    add modal in reminder

--- lib/screens/routine/reminder_routine.dart
+++ lib/screens/routine/reminder_routine.dart
@@ -1,14 +1,45 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+class RemeinderScreen extends StatefulWidget {
+  const RemeinderScreen({super.key});
 
-class RemeinderScreen extends StatelessWidget {
-const RemeinderScreen({super.key});
+  @override
+  State<RemeinderScreen> createState() => _RemeinderScreenState();
+}
+
+class _RemeinderScreenState extends State<RemeinderScreen> {
+  _open() {
+    final radius = Radius.circular(20);
+    final roundedRectangleBorder = RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: radius, topRight: radius));
+    showModalBottomSheet(
+      shape: roundedRectangleBorder,
+      context: context,
+      builder: (context) {
+        return Column(
+          children: const [Text('data')],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 1), _open);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('RemeinderScreen'),
-     ),
-   );
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+           TextButton(onPressed: _open, child: Text('open modal'))
+        ],
+      ),
+    );
   }
 }
